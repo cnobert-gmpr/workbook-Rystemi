624c86a2f3248c4622c8fa72c5c9c88b8ac553e4
Rystemi <lolgamer6205@gmail.com>
2026-03-01 22:35:40 -0700

    update?

---
+++ GMPR2512 Project/Assets/GMPR2512/Lesson 06/Flipper.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace GMPR2512.Lesson06
+
+{
+    public class Flipper : MonoBehaviour
+    {
+        private HingeJoint2D _joint2D;
+        [SerializeField] private bool _rightFlipper;
+        void Awake()
+        {
+            _joint2D = GetComponent<HingeJoint2D>();
+        }
+        
+        void Update()
+        {
+            if (_rightFlipper)
+                _joint2D.useMotor = Input.GetKey(KeyCode.RightShift);
+            else
+                _joint2D.useMotor = Input.GetKey(KeyCode.LeftShift);
+        }
+    }
+}
