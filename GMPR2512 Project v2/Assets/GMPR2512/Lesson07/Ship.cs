1ddeabe14bdc9659d3927c63eccf73d311838fea
Rystemi <lolgamer6205@gmail.com>
2026-03-02 14:42:41 -0700

    Lesson07 Day 1

--- GMPR2512 Project v2/Assets/GMPR2512/Lesson07/Ship.cs
+++ GMPR2512 Project v2/Assets/GMPR2512/Lesson07/Ship.cs
@@ -5,19 +5,63 @@ namespace GMPR2512.Lesson07
 {
     public class Ship : MonoBehaviour
     {
-        [SerializeField] private float _movementSpeed = 5;
+        [SerializeField] private float _movementSpeed = 5, _rotationSpeed = 500, _scaleSpeed = 1;
+        [SerializeField] private float _minRotation = -25, _maxRotation = 25;
 
-        private InputAction _moveAction;
+        private InputAction _moveAction, _rotationAction, _scaleAction;
 
         void Awake()
         {
             // this creates an input method that is decoupled from the input device
             _moveAction = InputSystem.actions.FindAction("Player/Move");
+            _rotationAction = InputSystem.actions.FindAction("Player/Move");
+            _scaleAction = InputSystem.actions.FindAction("Player/Scale");
         }
         void Update()
         {
-            Vector2 moveDirection = _moveAction.ReadValue<Vector2>();
-            transform.Translate(new Vector3(-Time.deltaTime,Time.deltaTime,0));
+            #region movement
+            Vector2 moveDirection = new Vector2(_moveAction.ReadValue<Vector2>().x, 0);
+            Vector2 translation = moveDirection.normalized * _movementSpeed * Time.deltaTime;
+            transform.Translate(translation, Space.Self);
+            #endregion
+
+            #region rotation
+            float rotationValue = _rotationAction.ReadValue<Vector2>().normalized.y * _rotationSpeed * Time.deltaTime;
+            transform.Rotate(0, 0, rotationValue, Space.Self);
+
+            Vector3 euler = transform.eulerAngles;
+
+            // Convert to signed range (-180 to 180)
+            if (euler.z > 180f)
+            {
+                euler.z -= 360f;
+            }
+            // Clamp, then assign back
+            euler.z = Mathf.Clamp(euler.z, _minRotation, _maxRotation);
+            transform.eulerAngles = euler;
+            #endregion
+
+            #region scaling
+            float scaleValue = _scaleAction.ReadValue<float>() * _scaleSpeed * Time.deltaTime;
+            Vector3 scaleChange = new Vector3(scaleValue, scaleValue, scaleValue);
+            transform.localScale += scaleChange;
+
+            Vector3 scale = transform.localScale;
+            if (scale.x < 0)
+            {
+                scale.x = 0;
+            }
+            if (scale.y < 0)
+            {
+                scale.y = 0;
+            }
+            if (scale.z < 0)
+            {
+                scale.z = 0;
+            }
+            transform.localScale = scale;
+            #endregion
+
         }
     }
 }
